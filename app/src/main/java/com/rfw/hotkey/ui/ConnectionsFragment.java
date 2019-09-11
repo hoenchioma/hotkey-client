a7d1820618949fd63fda4e4617683da5fdd18ccb
Raheeb Hassan <raheeb998@gmail.com>
2019-09-11 21:12:59 +0600

    setup basic UI

---
+++ app/src/main/java/com/rfw/hotkey/ui/ConnectionsFragment.java
@@ -0,0 +1,20 @@
+package com.rfw.hotkey.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.rfw.hotkey.R;
+
+public class ConnectionsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_connections, container, false);
+    }
+}
--- app/src/main/java/com/rfw/hotkey/ui/MainActivity.java
+++ app/src/main/java/com/rfw/hotkey/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.rfw.hotkey;
+package com.rfw.hotkey.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.rfw.hotkey.R;
 
 public class MainActivity extends AppCompatActivity {
 
