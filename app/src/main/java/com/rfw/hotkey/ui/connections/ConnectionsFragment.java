c9ea420e3af6f95498f1df9964f6e3fa680a28e9
Raheeb Hassan <raheeb998@gmail.com>
2019-10-29 05:13:11 +0600

    implemented QR code scanning to connect, redid connection fragment to integrate qr code

--- app/src/main/java/com/rfw/hotkey/ui/connections/ConnectionsFragment.java
+++ app/src/main/java/com/rfw/hotkey/ui/connections/ConnectionsFragment.java
@@ -2,31 +2,37 @@ package com.rfw.hotkey.ui.connections;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.preference.PreferenceManager;
 
+import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.textfield.TextInputLayout;
 import com.rfw.hotkey.R;
 import com.rfw.hotkey.databinding.FragmentConnectionsBinding;
 import com.rfw.hotkey.net.Connection;
 import com.rfw.hotkey.net.ConnectionManager;
 import com.rfw.hotkey.net.WiFiConnection;
+import com.rfw.hotkey.ui.connections.ConnectionsViewModel.State;
 import com.rfw.hotkey.util.Constants;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import java.util.Objects;
 
@@ -35,17 +41,10 @@ import static com.rfw.hotkey.util.Utils.getIntPref;
 
 public class ConnectionsFragment extends Fragment {
     private static final String TAG = ConnectionsFragment.class.getCanonicalName();
+    private static final int QR_CODE_READER_ACTIVITY_REQUEST = 0;
 
     private View contextView;
-
-    // don't remove local fields of UI components (as they are required to save the state of Fragment)
-    private MaterialButton connectButton;
-    private TextInputLayout ipAddressTextField;
-    private EditText ipAddressEditText;
-    private TextInputLayout portTextField;
-    private EditText portEditText;
-    private ProgressBar connectingSpinner;
-    private TextView statusTextView;
+    private ConnectionsViewModel viewModel;
 
     @Nullable
     @Override
@@ -56,93 +55,167 @@ public class ConnectionsFragment extends Fragment {
         FragmentConnectionsBinding binding = FragmentConnectionsBinding.inflate(inflater, container, false);
         contextView = binding.getRoot();
 
-        // setup databinding for cm (connectionManager)
-        ConnectionManager connectionManager = ConnectionManager.getInstance();
-        binding.setCm(connectionManager);
+        initDataBinding(binding);
+        init(contextView);
+        loadSharedPref();
 
-        connectButton = contextView.findViewById(R.id.connectButton);
-        ipAddressTextField = contextView.findViewById(R.id.ipAddressTextField);
-        ipAddressEditText = contextView.findViewById(R.id.ipAddressEditText);
-        portTextField = contextView.findViewById(R.id.portTextField);
-        portEditText = contextView.findViewById(R.id.portEditText);
-        connectingSpinner = contextView.findViewById(R.id.connectingSpinner);
-        statusTextView = contextView.findViewById(R.id.statusTextView);
+        return contextView;
+    }
 
+    private void init(View contextView) {
+        MaterialButton connectButton = contextView.findViewById(R.id.connectButton);
+        MaterialButton scanQRCodeButton = contextView.findViewById(R.id.scanQRCodeButton);
+        MaterialButton enterIPButton = contextView.findViewById(R.id.enterIPButton);
+
+        enterIPButton.setOnClickListener(view -> viewModel.state.set(State.IP_INPUT));
+        scanQRCodeButton.setOnClickListener(view -> scanQRCodeAction());
         connectButton.setOnClickListener(view -> connectButtonAction());
+    }
 
+    private void initDataBinding(FragmentConnectionsBinding binding) {
+        viewModel = ViewModelProviders.of(Objects.requireNonNull(getActivity())).get(ConnectionsViewModel.class);
+//        viewModel.state.set(ConnectionManager.getInstance().isConnectionActive()? State.CONNECTED : State.NOT_CONNECTED);
+//        viewModel.computerName = ConnectionManager.getInstance().getComputerName();
+        binding.setViewModel(viewModel);
+    }
+
+    private void loadSharedPref() {
         // load ip address and port from shared pref
         SharedPreferences sharedPref = Objects.requireNonNull(getActivity()).getPreferences(Context.MODE_PRIVATE);
         String ipAddress = sharedPref.getString("ipAddress", null);
         String portText = sharedPref.getString("portText", null);
         if (ipAddress != null && portText != null) {
-            ipAddressEditText.setText(ipAddress);
-            portEditText.setText(portText);
+            viewModel.setIpAddress(ipAddress);
+            viewModel.setPortStr(portText);
         }
-
-        return contextView;
     }
 
-    @Override
-    public void onStop() {
+    private void saveSharedPref() {
         // save ip address and port on exit
-        String ipAddress = ipAddressEditText.getText().toString();
-        String portText = portEditText.getText().toString();
+        String ipAddress = viewModel.getIpAddress();
+        String portText = viewModel.getPortStr();
         SharedPreferences sharedPref = Objects.requireNonNull(getActivity()).getPreferences(Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
         editor.putString("ipAddress", ipAddress);
         editor.putString("portText", portText);
         editor.apply();
+    }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        assert getView() != null;
+        getView().setFocusableInTouchMode(true);
+        getView().requestFocus();
+        getView().setOnKeyListener((v, keyCode, event) -> {
+            if (keyCode == KeyEvent.KEYCODE_BACK) {
+                if (viewModel.state.get() == State.IP_INPUT) {
+                    viewModel.state.set(State.NOT_CONNECTED);
+                    return true;
+                }
+            }
+            return false;
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        processQRCodeResult(resultCode, requestCode, data);
+    }
+
+    @Override
+    public void onStop() {
+        saveSharedPref();
         super.onStop();
+    }
+
+    private void scanQRCodeAction() {
+        Intent intent = QRCodeReaderActivity.getLaunchIntent(getContext(), true, false, barcode -> {
+            try {
+                JSONObject code = new JSONObject(new JSONTokener(barcode.rawValue));
+                return code.getString("type").equals("serverInfo");
+            } catch (JSONException e) {
+                return false;
+            }
+        });
+        startActivityForResult(intent, QR_CODE_READER_ACTIVITY_REQUEST);
+    }
+
+    private void processQRCodeResult(int resultCode, int requestCode, @Nullable Intent data) {
+        if (requestCode == QR_CODE_READER_ACTIVITY_REQUEST && data != null) {
+            if (resultCode == Activity.RESULT_OK) {
+                Barcode barcode = data.getParcelableExtra(QRCodeReaderActivity.KEY_CAPTURED_BARCODE);
+                assert barcode != null;
+                try {
+                    JSONObject code = new JSONObject(new JSONTokener(barcode.rawValue));
+                    if (!code.getString("type").equals("serverInfo")) throw new AssertionError();
+                    String ipAddress = code.getString("ipAddress");
+                    int port = code.getInt("port");
+                    makeConnection(ipAddress, port);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                String error = data.getStringExtra(QRCodeReaderActivity.KEY_ERROR);
+                Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), error, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void makeConnection(String ipAddress, int port) {
+        viewModel.state.set(State.CONNECTING);
+        long startTime = System.nanoTime();
+
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(Objects.requireNonNull(getActivity()));
+        int connectTimeOut = getIntPref(sharedPref, getString(R.string.settings_key_connect_timeout), Constants.Net.SOCKET_CONNECT_TIMEOUT);
+
+        Connection connection = new WiFiConnection(ipAddress, port, connectTimeOut) {
+            Activity activity = getActivity();
+
+            @Override
+            public void onConnect(boolean success, String errorMessage) {
+                try {
+                    // make sure the spinner is visible for at least 500 ms
+                    long currentTime = (System.nanoTime() - startTime);
+                    Thread.sleep(Math.max(500 - currentTime / 1000000, 0));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                viewModel.computerName = getComputerName();
+                viewModel.state.set(State.CONNECTED);
+
+//                        Snackbar.make(activity.findViewById(android.R.id.content),
+//                                success ? R.string.connection_success_msg : R.string.connection_error_msg,
+//                                Snackbar.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDisconnect() {
+                viewModel.state.set(State.NOT_CONNECTED);
+
+                Snackbar.make(activity.findViewById(android.R.id.content),
+                        R.string.connection_closed_msg,
+                        Snackbar.LENGTH_SHORT).show();
+            }
+        };
+        ConnectionManager.getInstance().makeConnection(connection);
     }
 
     private void connectButtonAction() {
         try {
             if (!ConnectionManager.getInstance().isConnectionActive()) {
-                String ipAddress = Objects.requireNonNull(ipAddressTextField.getEditText()).getText().toString();
+                String ipAddress = viewModel.getIpAddress();
                 if (ipAddress.isEmpty()) throw new RuntimeException("IP Address field empty");
-                String portText = Objects.requireNonNull(portTextField.getEditText()).getText().toString();
+                String portText = viewModel.getPortStr();
                 if (portText.isEmpty()) throw new RuntimeException("Port field empty");
                 int port = Integer.parseInt(portText);
 
-                statusTextView.setVisibility(View.GONE); // hide text view
-                connectingSpinner.setVisibility(View.VISIBLE); // show loading spinner
-                long startTime = System.nanoTime();
-
-                SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(Objects.requireNonNull(getActivity()));
-                int connectTimeOut = getIntPref(sharedPref, getString(R.string.settings_key_connect_timeout), Constants.Net.SOCKET_CONNECT_TIMEOUT);
-
-                Connection connection = new WiFiConnection(ipAddress, port, connectTimeOut) {
-                    Activity activity = getActivity();
-
-                    @Override
-                    public void onConnect(boolean success, String errorMessage) {
-                        try {
-                            // make sure the spinner is visible for at least 500 ms
-                            long currentTime = (System.nanoTime() - startTime);
-                            Thread.sleep(Math.max(500 - currentTime / 1000000, 0));
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-
-                        connectingSpinner.setVisibility(View.GONE); // hide loading spinner
-                        statusTextView.setVisibility(View.VISIBLE); // show status text view
-
-                        Snackbar.make(activity.findViewById(android.R.id.content),
-                                success ? R.string.connection_success_msg : R.string.connection_error_msg,
-                                Snackbar.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onDisconnect() {
-                        Snackbar.make(activity.findViewById(android.R.id.content),
-                                R.string.connection_closed_msg,
-                                Snackbar.LENGTH_SHORT).show();
-                    }
-                };
-                ConnectionManager.getInstance().makeConnection(connection);
+                makeConnection(ipAddress, port);
             } else { // if already connected disconnect on click
                 ConnectionManager.getInstance().closeConnection();
+                viewModel.state.set(State.NOT_CONNECTED);
             }
         } catch (RuntimeException e) {
             // show a snackbar if an error occurred
