c9d590df9fe1e3c78fcb45a145667e7256c136f2
Raheeb Hassan <raheeb998@gmail.com>
2019-11-20 17:33:28 +0600

    Optimized imports in MultimediaFragment

--- app/src/main/java/com/rfw/hotkey/ui/multimedia/MultimediaFragment.java
+++ app/src/main/java/com/rfw/hotkey/ui/multimedia/MultimediaFragment.java
@@ -1,28 +1,19 @@
 package com.rfw.hotkey.ui.multimedia;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.annotation.DrawableRes;
-import androidx.fragment.app.Fragment;
-
-import android.util.JsonReader;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.SeekBar;
-import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
 
 import com.rfw.hotkey.R;
 import com.rfw.hotkey.net.ConnectionManager;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.logging.Level;
 
 
 public class MultimediaFragment extends Fragment {
