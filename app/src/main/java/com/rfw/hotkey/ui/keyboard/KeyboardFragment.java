22ea91f1587cb30293e561b89636ce9a624c6042
Shadman Wadith <wadith.24csedu.027@gmail.com>
2019-12-06 03:28:45 +0600

    Keyboard UI updated

--- app/src/main/java/com/rfw/hotkey/ui/keyboard/KeyboardFragment.java
+++ app/src/main/java/com/rfw/hotkey/ui/keyboard/KeyboardFragment.java
@@ -7,7 +7,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -25,24 +27,24 @@ import org.json.JSONObject;
  *
  * @author Shadman Wadith
  */
-public class KeyboardFragment extends Fragment implements View.OnClickListener {
+public class KeyboardFragment extends Fragment implements View.OnClickListener ,View.OnLongClickListener{
 
     private static final String TAG = KeyboardFragment.class.getCanonicalName();
 
     private KeyboardView keyboardView;
 
-    private Button copyButton;
-    private Button escButton;
-    private Button homeButton;
-    private Button tabButton;
-    private Button pasteButton;
-    private Button pgupButton;
-    private Button shiftButton;
-    private Button upButton;
-    private Button pgdnButton;
-    private Button leftButton;
-    private Button downButton;
-    private Button righButton;
+    private ImageButton copyButton;
+    private ImageButton escButton;
+    private ImageButton homeButton;
+    private ImageButton tabButton;
+    private ImageButton pasteButton;
+    private ImageButton pgupButton;
+    private ImageButton shiftButton;
+    private ImageButton upButton;
+    private ImageButton pgdnButton;
+    private ImageButton leftButton;
+    private ImageButton downButton;
+    private ImageButton righButton;
 
     private LinearLayout emptySpace;
 
@@ -94,10 +96,10 @@ public class KeyboardFragment extends Fragment implements View.OnClickListener {
         homeButton = rootView.findViewById(R.id.homeButtonID);
         tabButton = rootView.findViewById(R.id.tabButtonID);
         pasteButton = rootView.findViewById(R.id.pasteButtonID);
-        pgupButton = rootView.findViewById(R.id.pgupButtonID);
-        shiftButton = rootView.findViewById(R.id.shiftButtonID);
+//        pgupButton = rootView.findViewById(R.id.pgupButtonID);
+//        shiftButton = rootView.findViewById(R.id.shiftButtonID);
         upButton = rootView.findViewById(R.id.upButtonID);
-        pgdnButton = rootView.findViewById(R.id.pgdnButtonID);
+//        pgdnButton = rootView.findViewById(R.id.pgdnButtonID);
         leftButton = rootView.findViewById(R.id.leftButtonID);
         downButton = rootView.findViewById(R.id.downButtonID);
         righButton = rootView.findViewById(R.id.rightButtonID);
@@ -107,14 +109,28 @@ public class KeyboardFragment extends Fragment implements View.OnClickListener {
         escButton.setOnClickListener(this);
         tabButton.setOnClickListener(this);
         pasteButton.setOnClickListener(this);
-        pgupButton.setOnClickListener(this);
+ //     pgupButton.setOnClickListener(this);
+//      pgdnButton.setOnClickListener(this);
         upButton.setOnClickListener(this);
-        pgdnButton.setOnClickListener(this);
         leftButton.setOnClickListener(this);
         downButton.setOnClickListener(this);
         righButton.setOnClickListener(this);
 //      shiftButton.setOnTouchListener(this);
 //      rootView.setOnTouchListener(this);
+    
+        copyButton.setOnLongClickListener(this);
+//      homeButton.setOnTouchListener(this);
+        homeButton.setOnLongClickListener(this);
+        escButton.setOnLongClickListener(this);
+        tabButton.setOnLongClickListener(this);
+        pasteButton.setOnLongClickListener(this);
+        //     pgupButton.setOnLongClickListener(this);
+//      pgdnButton.setOnLongClickListener(this);
+        upButton.setOnLongClickListener(this);
+        leftButton.setOnLongClickListener(this);
+        downButton.setOnLongClickListener(this);
+        righButton.setOnLongClickListener(this);
+//      shiftButton.setOnTouchListener(this);
     }
 
 
@@ -124,27 +140,32 @@ public class KeyboardFragment extends Fragment implements View.OnClickListener {
             case R.id.escButtonID:
                 sendKeyToServer("modifier", "ESC");
                 Log.d("onclick", "ESC");
+                Toast.makeText(getActivity(), "ESC", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.copyButtonID:
                 sendKeyToServer("command", "COPY");
+                Toast.makeText(getActivity(), "Copy", Toast.LENGTH_SHORT).show();
                 Log.d("onclick", "COPY");
                 break;
             case R.id.homeButtonID:
                 sendKeyToServer("modifier", "HOME");
+                Toast.makeText(getActivity(), "Home", Toast.LENGTH_SHORT).show();
                 Log.d("onclick", "HOME");
                 break;
             case R.id.tabButtonID:
                 sendKeyToServer("modifier", "TAB");
+                Toast.makeText(getActivity(), "TAB", Toast.LENGTH_SHORT).show();
                 Log.d("onclick", "TAB");
                 break;
             case R.id.pasteButtonID:
                 sendKeyToServer("command", "PASTE");
+                Toast.makeText(getActivity(), "Paste", Toast.LENGTH_SHORT).show();
                 Log.d("onclick", "PASTE");
                 break;
-            case R.id.pgupButtonID:
-                sendKeyToServer("modifier", "PGUP");
-                Log.d("onclick", "PGUP");
-                break;
+//            case R.id.pgupButtonID:
+//                sendKeyToServer("modifier", "PGUP");
+//                Log.d("onclick", "PGUP");
+//                break;
 //            case R.id.shiftButtonID:
 //                sendKeyToServer("TYPE_HOLD");
 //                sendKeyToServer("SHIFT");
@@ -152,22 +173,26 @@ public class KeyboardFragment extends Fragment implements View.OnClickListener {
 //                break;
             case R.id.upButtonID:
                 sendKeyToServer("modifier", "UP");
+                Toast.makeText(getActivity(), "Up Arrow", Toast.LENGTH_SHORT).show();
                 Log.d("onclick", "UP");
                 break;
-            case R.id.pgdnButtonID:
-                sendKeyToServer("modifier", "PGDN");
-                Log.d("onclick", "PGDN");
-                break;
+//            case R.id.pgdnButtonID:
+//                sendKeyToServer("modifier", "PGDN");
+//                Log.d("onclick", "PGDN");
+//                break;
             case R.id.leftButtonID:
                 sendKeyToServer("modifier", "LEFT");
+                Toast.makeText(getActivity(), "Left Arrow", Toast.LENGTH_SHORT).show();
                 Log.d("onclick", "LEFT");
                 break;
             case R.id.downButtonID:
                 sendKeyToServer("modifier", "DOWN");
+                Toast.makeText(getActivity(), "Down Arrow", Toast.LENGTH_SHORT).show();
                 Log.d("onclick", "DOWN");
                 break;
             case R.id.rightButtonID:
                 sendKeyToServer("modifier", "RIGHT");
+                Toast.makeText(getActivity(), "Right Arrow", Toast.LENGTH_SHORT).show();
                 Log.d("onclick", "RIGHT");
                 break;
         }
@@ -191,4 +216,48 @@ public class KeyboardFragment extends Fragment implements View.OnClickListener {
         ConnectionManager.getInstance().sendPacket(packet);
     }
 
+    @Override
+    public boolean onLongClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.escButtonID:
+                Toast.makeText(getActivity(), "ESC", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.copyButtonID:
+                Toast.makeText(getActivity(), "Copy", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.homeButtonID:
+                Toast.makeText(getActivity(), "Home", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tabButtonID:
+                Toast.makeText(getActivity(), "TAB", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.pasteButtonID:
+                Toast.makeText(getActivity(), "Paste", Toast.LENGTH_SHORT).show();
+                break;
+//            case R.id.pgupButtonID:
+//                 break;
+//            case R.id.shiftButtonID:
+//
+//                break;
+            case R.id.upButtonID:
+                Toast.makeText(getActivity(), "Up Arrow", Toast.LENGTH_SHORT).show();
+
+                break;
+//            case R.id.pgdnButtonID:
+//                sendKeyToServer("modifier", "PGDN");
+//                Log.d("onclick", "PGDN");
+//                break;
+            case R.id.leftButtonID:
+                Toast.makeText(getActivity(), "Left Arrow", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.downButtonID:
+                Toast.makeText(getActivity(), "Down Arrow", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.rightButtonID:
+                Toast.makeText(getActivity(), "Right Arrow", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return false;
+    }
 }
