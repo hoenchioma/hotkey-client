8940d1e9592ea7916421ac467d4f70680cdf22a3
Raheeb Hassan <raheeb998@gmail.com>
2019-12-11 21:12:56 +0600

    Fixed characters in gamepad selection

--- app/src/main/java/com/rfw/hotkey/ui/gamepad/GamepadActivity.java
+++ app/src/main/java/com/rfw/hotkey/ui/gamepad/GamepadActivity.java
@@ -74,13 +74,14 @@ public class GamepadActivity extends AppCompatActivity {
             "ESC", "ALT", "CTRL", "SHIFT", "DEL",
             "INS", "HOME", "END", "PGUP", "PGDN",
 
-            "A", "b", "c", "D", "E",
-            "f", "g", "h", "i", "j",
-            "k", "l", "m", "n", "o",
-            "p", "q", "r", "S", "t",
-            "u", "v", "W", "x", "y",
-            "z","UP","DOWN","LEFT","RIGHT"
+            "A", "B", "C", "D", "E",
+            "F", "G", "H", "I", "J",
+            "K", "L", "M", "N", "O",
+            "P", "Q", "R", "S", "T",
+            "U", "V", "W", "X", "Y",
+            "Z",
 
+            "UP","DOWN","LEFT","RIGHT"
     };
 
     final List<String> gridKeys = new ArrayList<String>(Arrays.asList(keyboardKeys));
